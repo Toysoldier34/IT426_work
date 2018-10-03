f3a8e8476168fc4134ba17cf04f7c4e7068766fc
Tony <tony.g.thompson@gmail.com>
2018-10-03 00:36:55 -0700

    Buttons all displaying properly in UI, code cleaned up

--- Calculator JavaFX/src/ui/CalculatorUI.java
+++ Calculator JavaFX/src/ui/CalculatorUI.java
@@ -5,27 +5,29 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 public class CalculatorUI extends Application{
 
 
     private static final int NUM_COLS = 4;
     private static final int TOTAL_COLS = 4;
-    private static final int WIN_WIDTH = 500;
-    private static final int WIN_HEIGHT = 500;
+    private static final int WIN_WIDTH = 200;
+    private static final int WIN_HEIGHT = 225;
     private static final int H_GAP = 10;
     private static final int V_GAP = 10;
-    private static final int PADDING = 40;
-    public static final int BUTTON_SMALL = 1;
-    public static final int COL_WIDTH = 40;
+    private static final int PADDING = 25;
+    private static final int BUTTON_SMALL = 1;
+    private static final int COL_WIDTH = 30;
+    private static final String[] BUTTON_LABELS = new String[]{
+            "7", "8", "9", "+",
+            "4", "5", "6", "-",
+            "1", "2", "3", "*",
+            "0", "Enter", null, "/"
+    };
 
 
     /**
@@ -50,12 +52,12 @@ public class CalculatorUI extends Application{
         primaryStage.show();
     }
 
+    //Setup Scene
     private Scene assembleScene() {
         GridPane panel = new GridPane();
         setPanelStyle(panel);
         columnConstraints(panel);
-        createButtons(panel);
-        createTextArea(panel);
+        createAllButtons(panel);
         return new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
     }
 
@@ -63,8 +65,8 @@ public class CalculatorUI extends Application{
     private void setPanelStyle(GridPane panel) {
         panel.setHgap(H_GAP);
         panel.setVgap(V_GAP);
-        //panel.setGridLinesVisible(true);
         panel.setPadding(new Insets(PADDING));
+        //panel.setGridLinesVisible(true);
     }
 
     //Column Constraints
@@ -76,90 +78,44 @@ public class CalculatorUI extends Application{
         }
     }
 
-
+    //Create Text Display
     private void createTextArea(GridPane panel) {
         TextField field = new TextField();
         panel.add(field, 0, 4, 4, 1);
     }
 
 
-    //Create Buttons
-    private void createButtons(GridPane panel) {
-        //createNumberButtons(panel);
-        //other buttons here
-        String[] buttonLabels = {
-                "7", "8", "9", "+",
-                "4", "5", "6", "-",
-                "1", "2", "3", "*",
-                "0", null, null, "/"
-        };
-        for (int i = 0; i < buttonLabels.length-1; i++) {
-            addNewButton(panel, newButton(buttonLabels[i]), i);
+    //Create all Buttons
+    private void createAllButtons(GridPane panel) {
+        for (int i = 0; i < BUTTON_LABELS.length; i++) {
+            setButtonSize(panel, newButton(BUTTON_LABELS[i]), i);
         }
-
-        addNewButton(panel, newButton("Enter"), 13);
+        createTextArea(panel);
     }
 
+    //Create a new Button
     private Button newButton(String text) {
         return (text != null) ? new Button(""+text) : null;
     }
 
-
-    private void createNumberButtons(GridPane panel) {
-        for (int i = 0; i < 10; i++ ) {
-            int displayNum = Math.abs(i - 9);
-            Button numberButton = new Button(""+ displayNum);
-            addNewButton(panel, numberButton, i, BUTTON_SMALL, BUTTON_SMALL);
-            //TODO setMaxButtonSize(numberButton);
-        }
-    }
-
-
-    private void addNewButton(GridPane panel, Button button, int buttonPositionNum) {
+    //calls add after determining if size change is needed
+    private void setButtonSize(GridPane panel, Button button, int buttonPositionNum) {
+        int buttonSize = BUTTON_SMALL;
         if (button != null) {  //skips large button parts
-            addNewButton(panel, button, buttonPositionNum, BUTTON_SMALL, BUTTON_SMALL);
+            if (button.getText().equals("Enter")) buttonSize = 2;
+            addButtonToPanel(panel, button, buttonPositionNum, buttonSize, BUTTON_SMALL);
         }
     }
-    private void addNewButton(GridPane panel, Button button, int buttonPositionNum, int cSpan, int rSpan) {
+
+    //Adds button to panel
+    private void addButtonToPanel(GridPane panel, Button button, int buttonPositionNum, int cSpan, int rSpan) {
         panel.add(button, buttonPositionNum % TOTAL_COLS, buttonPositionNum / TOTAL_COLS, cSpan, rSpan);
-//calcButtonColumn((buttonPositionNum +2) % 3)
-    }
-
-    //add 2 to num before mod
-
-    private int calcButtonColumn(int position) {
-        switch (position % NUM_COLS) {
-            case 0:  return 2;
-            case 1:  return 1;
-            default: return 0;
-        }
-    }
-
-    private void setMaxButtonSize(Button button) {
         setMaxButtonWidth(button);
-        setMaxButtonHeight(button);
     }
 
+    //Sets button width to max value
     private void setMaxButtonWidth(Button button) {
         button.setPrefWidth(Double.MAX_VALUE);
     }
-
-    private void setMaxButtonHeight(Button button) {
-        button.setPrefHeight(Double.MAX_VALUE);
-    }
-
-
-/*
-    private void test() {
-        testArray =
-        //TODO add code that manually builds calc, can reduce later
-        //TODO create 2d array with strings of numbers
-        //TODO add button class
-
-                ["text", ]
-        addNewButton();
-    }
-*/
-
 
 }
