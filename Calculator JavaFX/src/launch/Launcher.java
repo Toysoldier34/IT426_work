c8606955bf137098d10acf561379ed7d99212d8b
Tony <tony.g.thompson@gmail.com>
2018-10-03 00:52:45 -0700

    Launcher implemented and functioning with UI

--- Calculator JavaFX/src/launch/Launcher.java
+++ Calculator JavaFX/src/launch/Launcher.java
@@ -1,4 +1,20 @@
 package launch;
 
+import javafx.application.Application;
+import userinterface.CalculatorUI;
+
+/**
+ * Launcher for Calculator program
+ * @author Tony Thompson
+ * @version 1.0
+ */
 public class Launcher {
+    /**
+     * Main method to launch Calculator
+     * @param args no parameters
+     */
+    public static void main(String[] args) {
+        Application.launch(CalculatorUI.class, args);
+    }
+
 }
