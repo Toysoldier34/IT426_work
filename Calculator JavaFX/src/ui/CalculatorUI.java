df662a204390dde9cb87199965edcd450bc3e554
Tony <tony.g.thompson@gmail.com>
2018-10-02 23:26:13 -0700

    Making progress with button display

--- Calculator JavaFX/src/ui/CalculatorUI.java
+++ Calculator JavaFX/src/ui/CalculatorUI.java
@@ -5,22 +5,27 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class CalculatorUI extends Application{
 
 
-    private static final int NUM_COLS = 3;
+    private static final int NUM_COLS = 4;
     private static final int TOTAL_COLS = 4;
-    private static final int NUM_ROWS = 3;
     private static final int WIN_WIDTH = 500;
     private static final int WIN_HEIGHT = 500;
     private static final int H_GAP = 10;
     private static final int V_GAP = 10;
     private static final int PADDING = 40;
     public static final int BUTTON_SMALL = 1;
+    public static final int COL_WIDTH = 40;
 
 
     /**
@@ -50,7 +55,7 @@ public class CalculatorUI extends Application{
         setPanelStyle(panel);
         columnConstraints(panel);
         createButtons(panel);
-        createText
+        createTextArea(panel);
         return new Scene(panel, WIN_WIDTH, WIN_HEIGHT);
     }
 
@@ -58,7 +63,7 @@ public class CalculatorUI extends Application{
     private void setPanelStyle(GridPane panel) {
         panel.setHgap(H_GAP);
         panel.setVgap(V_GAP);
-        panel.setGridLinesVisible(true);
+        //panel.setGridLinesVisible(true);
         panel.setPadding(new Insets(PADDING));
     }
 
@@ -67,14 +72,36 @@ public class CalculatorUI extends Application{
         ObservableList<ColumnConstraints> columns = panel.getColumnConstraints();
         for (int i = 0; i < TOTAL_COLS; i++)
         {
-            columns.add(new ColumnConstraints(100));
+            columns.add(new ColumnConstraints(COL_WIDTH));
         }
     }
 
+
+    private void createTextArea(GridPane panel) {
+        TextField field = new TextField();
+        panel.add(field, 0, 4, 4, 1);
+    }
+
+
     //Create Buttons
     private void createButtons(GridPane panel) {
-        createNumberButtons(panel);
+        //createNumberButtons(panel);
         //other buttons here
+        String[] buttonLabels = {
+                "7", "8", "9", "+",
+                "4", "5", "6", "-",
+                "1", "2", "3", "*",
+                "0", null, null, "/"
+        };
+        for (int i = 0; i < buttonLabels.length-1; i++) {
+            addNewButton(panel, newButton(buttonLabels[i]), i);
+        }
+
+        addNewButton(panel, newButton("Enter"), 13);
+    }
+
+    private Button newButton(String text) {
+        return (text != null) ? new Button(""+text) : null;
     }
 
 
@@ -88,18 +115,13 @@ public class CalculatorUI extends Application{
     }
 
 
-    private void test() {
-        testArray =
-        //TODO add code that manually builds calc, can reduce later
-        //TODO create 2d array with strings of numbers
-        //TODO add button class
-
-                ["text", ]
-        addNewButton();
+    private void addNewButton(GridPane panel, Button button, int buttonPositionNum) {
+        if (button != null) {  //skips large button parts
+            addNewButton(panel, button, buttonPositionNum, BUTTON_SMALL, BUTTON_SMALL);
+        }
     }
-
     private void addNewButton(GridPane panel, Button button, int buttonPositionNum, int cSpan, int rSpan) {
-        panel.add(button, (buttonPositionNum +2 )% 3, buttonPositionNum / 3, cSpan, rSpan);
+        panel.add(button, buttonPositionNum % TOTAL_COLS, buttonPositionNum / TOTAL_COLS, cSpan, rSpan);
 //calcButtonColumn((buttonPositionNum +2) % 3)
     }
 
@@ -127,6 +149,17 @@ public class CalculatorUI extends Application{
     }
 
 
+/*
+    private void test() {
+        testArray =
+        //TODO add code that manually builds calc, can reduce later
+        //TODO create 2d array with strings of numbers
+        //TODO add button class
+
+                ["text", ]
+        addNewButton();
+    }
+*/
 
 
 }
